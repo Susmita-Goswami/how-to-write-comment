06e01477dcf260ac62b77cc93fbd7edcc99dcc46
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-24 17:45:23 +0600

    Add files via upload

---
+++ Comment.c
@@ -0,0 +1,10 @@
+//here i'm saying my name
+/*and my name is susmita goswami*/
+//for one line comment
+/* for multiple line comment*/
+#include<stdio.h>
+int main(void)
+{
+    printf("my name is Susmita Goswami");
+
+}
